b6476304415c8ed767ae605bdbd88ccaec4dd3fe
Guangyiyang <42775312+UCASY@users.noreply.github.com>
2019-05-10 10:08:30 +0800

    Update READ_STRING.F90

--- READ_STRING.F90
+++ READ_STRING.F90
@@ -2,9 +2,9 @@ PROGRAM UCASY
   IMPLICIT NONE
   INTEGER :: I, TIME, HOURS, MINUTES
   TIME = "9:15"
-  I = INDEX(C,':')
-  READ(C(:I-1),*) HOURS    ! String to integer
-  READ(C(I+1:),*) MINUTES  ! String to integer
+  I = INDEX(TIME,':')
+  READ(TIME(:I-1),*) HOURS    ! String to integer
+  READ(TIME(I+1:),*) MINUTES  ! String to integer
   WRITE(*,*) HOURS,MINUTES
   STOP
 END PROGRAM
